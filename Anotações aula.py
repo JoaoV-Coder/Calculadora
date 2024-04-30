e10f71c1a8cfcf2e6b91a9d76e15d6be4d252374
João Victor <joaovps41@gmail.com>
2024-04-30 11:03:15 -0300

    Projeto inicial

---
+++ Anotações aula.py
@@ -0,0 +1,35 @@
+nome = "Ana Maria"
+idade = 17
+
+print(f"O nome da aluna é {nome} e sua idade é {idade} anos.")
+
+#f - sever para formartar o codigo
+
+nome_aluno = 'Fabricio Daniel'
+idade_aluno = 15
+media_aluno = 8.45
+
+print('Nome do aluno é %s, ele tem %d anos e sua média é %f.' %(nome_aluno, idade_aluno, media_aluno))
+
+#s - String
+#d - inteiro
+#f - float
+
+idade_maria = int(input('Digite a idade da Maria: '))
+idade_beatriz = int(input('Digite a idade da Beatriz: '))
+
+if idade_maria > idade_beatriz:
+  ('Maria é mais velha que Beatriz.')
+
+
+media = float(input('Digite a média: '))
+
+if media >= 6.0:
+  print('Aprovado(a)')
+if 6.0 > media >= 4.0:
+  print('Recuperação')
+if media < 4.0:
+  print('Reprovado(a)')
+
+
+
